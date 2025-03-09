a8f1d6c950f86222a6a144801f3e96ce049b8463
JoseRD149 <joseromerodiaz89@gmail.com>
2025-03-09 20:07:04 +0100

    Refactoring dnaToRna to reduce cyclomatic complexity

--- rnatranscription/src/main/java/rnatranscription/RnaTranscription.java
+++ rnatranscription/src/main/java/rnatranscription/RnaTranscription.java
@@ -1,28 +1,24 @@
 package rnatranscription;
-
+import java.util.Map;
 public class RnaTranscription {
+
+    private static final Map<Character, Character> TRANSCRIPTION_MAP = Map.of(
+        'G', 'C',
+        'C', 'G',
+        'T', 'A',
+        'A', 'U'
+    );
 
     public static String dnaToRna(String dnaSequence) {
         StringBuilder rnaSequence = new StringBuilder();
-        for (int i = 0; i < dnaSequence.length(); i++) {
-            char nucleotide = dnaSequence.charAt(i);
-            switch (nucleotide) {
-                case 'G':
-                    rnaSequence.append('C');
-                    break;
-                case 'C':
-                    rnaSequence.append('G');
-                    break;
-                case 'T':
-                    rnaSequence.append('A');
-                    break;
-                case 'A':
-                    rnaSequence.append('U');
-                    break;
-                default:
-                    throw new IllegalArgumentException("Invalid DNA nucleotide: " + nucleotide);
+
+        for (char nucleotide : dnaSequence.toCharArray()) {
+            if (!TRANSCRIPTION_MAP.containsKey(nucleotide)) {
+                throw new IllegalArgumentException("Invalid DNA nucleotide: " + nucleotide);
             }
+            rnaSequence.append(TRANSCRIPTION_MAP.get(nucleotide));
         }
+
         return rnaSequence.toString();
     }
 }
