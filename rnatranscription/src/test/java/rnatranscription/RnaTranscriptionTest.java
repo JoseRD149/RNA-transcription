70be4cb2458fde689b7b209fb20603201fe8edda
JoseRD149 <joseromerodiaz89@gmail.com>
2025-03-09 20:11:04 +0100

    Added test for long DNA sequences

--- rnatranscription/src/test/java/rnatranscription/RnaTranscriptionTest.java
+++ rnatranscription/src/test/java/rnatranscription/RnaTranscriptionTest.java
@@ -24,11 +24,22 @@ public class RnaTranscriptionTest {
 
       @Test
     public void testInvalidNucleotideThrowsException() {
-        String invalidDna = "GXTA"; // 'X' no es un nucleótido válido
+        String invalidDna = "GXTA"; 
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             RnaTranscription.dnaToRna(invalidDna);
         });
 
         assertEquals("Invalid DNA nucleotide: X", exception.getMessage());
     }
+
+    @Test
+public void testLongDnaSequence() {
+    String dnaSequence = "GCTAGCTAGCTAGCTAGCTA"; 
+    String expectedRnaSequence = "CGAUCGAUCGAUCGAUCGAU"; 
+
+    String result = RnaTranscription.dnaToRna(dnaSequence);
+
+    assertEquals(expectedRnaSequence, result);
+}
+
 }
